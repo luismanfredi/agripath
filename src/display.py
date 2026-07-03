5cebcca0aa5e60d30fcd99aa2b6c3779f426a57c
luismanfredi <luismanfredi920@gmail.com>
2026-07-03 12:29:06 -0300

    feat: add mypy extension

--- src/display.py
+++ src/display.py
@@ -1,7 +1,7 @@
 from models.product import Product
 
 
-def print_history(product: Product):
+def print_history(product: Product) -> None:
     print(product)
     for item in product.events:
         print(item)
