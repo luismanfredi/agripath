293de56f6a224c757cd0b9850be0676955187be4
luismanfredi <luismanfredi920@gmail.com>
2026-07-12 17:15:38 -0300

    chore: remove env configs from database.py

--- src/agripath/db/database.py
+++ src/agripath/db/database.py
@@ -1,12 +1,7 @@
-import os
-
-from dotenv import load_dotenv
 from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-load_dotenv()
-
-DATABASE_URL: str | None = os.getenv("DATABASE_URL")
+from agripath.config import DATABASE_URL
 
 if DATABASE_URL is None:
     raise ValueError("DATABASE_URL was not found in .env")
